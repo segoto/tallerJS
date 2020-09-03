fd18f2af83904ea81b779eed48fe50b959a14e01
segoto <js.gomez23@uniandes.edu.co>
2020-09-03 00:11:09 -0500

    corrigiendo porque ajá

--- tarea.js
+++ tarea.js
@@ -3,4 +3,4 @@ function secret(arr, encryptDecrypt, n){
     return arr.map(a=> encryptDecrypt( a, n ));
 }
 //Punto 2
-let mcd = (a,b)=> a === b ? a : a < b ? mcd(a, b-a) : mcd(b, a-b);
+let mcd = (a,b)=> b === 0? a: mcd(b, a%b);
